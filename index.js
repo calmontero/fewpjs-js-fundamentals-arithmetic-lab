6d7c5a6029ab5fc8358ef6bb3cd70699a7afb8ce
Carlos <carlos.alfonso.montero@gmail.com>
2021-03-31 18:53:58 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31, num2 = 2, num3 = 19, num4 = 5;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(1,4,8,14,17,20);
